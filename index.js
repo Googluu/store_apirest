87dc00c9076ebb3d1b7568424e2cb2c11a9bf48c
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-05 10:49:25 -0500

    app up

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("My App");
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(port, () => {
+  console.log("listening on port");
+});
