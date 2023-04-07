6ab2f4beced6bd88a11a6052b7adbc1c92eec6ef
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-07 04:58:55 -0500

    endpoints

--- index.js
+++ index.js
@@ -9,9 +9,44 @@ app.get("/", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
+  res.json([
+    {
+      name: "product 1",
+      price: 1000,
+    },
+    {
+      name: "product 2",
+      price: 1000,
+    },
+  ]);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: "product 1",
+    id,
+    name: "product 2",
     price: 1000,
+  });
+});
+
+app.get("/users", (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send("No hay parametros");
+  }
+});
+
+app.get("/categories/:categoryId/products/:productId", (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
