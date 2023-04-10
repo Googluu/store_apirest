71226b598ed0fc80315b0860ba53d7870089e77c
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-10 14:53:52 -0500

    logica de negocio-sevicios

---
+++ app/services/products.service.js
@@ -0,0 +1,31 @@
+const createRandomProducts = require("../../data_faker/products_faker");
+
+class ProductsService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+    for (let i = 0; i < limit; i++) {
+      this.products.push(createRandomProducts());
+    }
+  }
+
+  create() {}
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  update() {}
+
+  delete() {}
+}
+
+module.exports = ProductsService;
