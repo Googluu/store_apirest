04067020c79ec9f2e119c143863fb374d9d9c42c
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-17 10:50:08 -0500

    controller user created

--- app/routes/users.js
+++ app/routes/users.js
@@ -16,37 +16,61 @@ router.get("/", async (req, res) => {
   res.json(users);
 });
 
-router.get("/:id", validatorHandler(findUserById, params), async (req, res) => {
-  const { id } = req.params;
-  const user = await service.findOne(id);
-  res.json(user);
-});
+router.get(
+  "/:id",
+  validatorHandler(findUserById, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-router.post("/", validatorHandler(createUser, body), async (req, res) => {
-  const body = req.body;
-  const user = await service.create(body);
-  res.status(201).json(user);
-});
+router.post(
+  "/",
+  validatorHandler(createUser, "body"),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const user = await service.create(body);
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 router.patch(
   "/:id",
-  validatorHandler(findUserById, params),
-  validatorHandler(updateUser, body),
-  async (req, res) => {
-    const { id } = req.params;
-    const body = req.body;
-    const user = await service.update(id, body);
-    res.json(user);
+  validatorHandler(findUserById, "params"),
+  validatorHandler(updateUser, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const user = await service.update(id, body);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 router.delete(
   "/:id",
-  validatorHandler(findUserById, params),
-  async (req, res) => {
-    const { id } = req.params;
-    const user = await service.delete(id);
-    res.status(204).json(user);
+  validatorHandler(findUserById, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.delete(id);
+      res.status(204).json(user);
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
