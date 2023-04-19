64480fb2b793f4829254bde287e63967d0979081
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-19 11:39:15 -0500

    setup sequelize-cli for migrations

--- db/config.js
+++ db/config.js
@@ -1,19 +1,16 @@
-const { Sequelize } = require("sequelize");
-
 const { config } = require("../config/config");
-const setupModels = require("../db/models");
 
 const USER = encodeURIComponent(config.dbPostgres.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPostgres.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbPostgres.dbHost}:${config.dbPostgres.dbPort}/${config.dbPostgres.dbName}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: "postgres",
-  logging: true,
-  // dialectModule: require("mysql2"),
-});
-
-setupModels(sequelize);
-sequelize.sync();
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    url: URI,
+    dialect: "postgres",
+  },
+  production: {
+    url: URI,
+    dialect: "postgres",
+  },
+};
