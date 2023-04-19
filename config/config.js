b27b3d540114f902f834645c17f5cf0e339c234a
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-19 11:18:37 -0500

    db-mysql for example the conection with docker

--- config/config.js
+++ config/config.js
@@ -3,11 +3,20 @@ require("dotenv").config();
 const config = {
   env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  dbPostgres: {
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+  },
+  dbMysql: {
+    dbUser: process.env.DB_MYSQL_USER,
+    dbPassword: process.env.DB_MYSQL_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_MYSQL_DATABASE,
+    dbPort: process.env.DB_MYSQL_PORT,
+  },
 };
 
 module.exports = { config };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,13 +3,14 @@ const { Sequelize } = require("sequelize");
 const { config } = require("../config/config");
 const setupModels = require("../db/models");
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const USER = encodeURIComponent(config.dbMysql.dbUser);
+const PASSWORD = encodeURIComponent(config.dbMysql.dbPassword);
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbMysql.dbHost}:${config.dbMysql.dbPort}/${config.dbMysql.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: "postgres",
+  dialect: "mysql",
   logging: true,
+  dialectModule: require("mysql2"),
 });
 
 setupModels(sequelize);
