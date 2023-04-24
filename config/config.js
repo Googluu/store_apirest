7e6511f87110136a3eb51df8c75b287d14a1f558
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-04-24 11:05:46 -0500

    sign-login-user generate-new-token

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ const config = {
   env: process.env.NODE_ENV || "dev",
   port: process.env.PORT || 3000,
   apikey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
   dbPostgres: {
     dbUser: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD,
