10e682f1532b1d18fa74a26977877e5967887184
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-05-22 08:29:49 -0500

    autenticacion token

--- app/graphql/category.resolvers.js
+++ app/graphql/category.resolvers.js
@@ -1,7 +1,11 @@
+const { unauthorized } = require("@hapi/boom");
+
 const CategoriesService = require("../services/categories.service");
 const service = new CategoriesService();
 
-const addCategory = (_, { dto }) => {
+const addCategory = async (_, { dto }, context) => {
+  const { user } = await context.authenticate("jwt", { session: false });
+  if (!user) throw unauthorized(`jwt is not valid`);
   return service.create(dto);
 };
 
